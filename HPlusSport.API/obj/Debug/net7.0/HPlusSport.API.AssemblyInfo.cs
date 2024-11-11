d9bf02ed6168d9d3e0edb7a56d40d5530c5df43a
Winifred Rex <77475621+winifredrex@users.noreply.github.com>
2024-11-11 09:38:09 +0100

    Finish

--- HPlusSport.API/obj/Debug/net7.0/HPlusSport.API.AssemblyInfo.cs
+++ HPlusSport.API/obj/Debug/net7.0/HPlusSport.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HPlusSport.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1e576482bb5a6293b3a2ee52b00884cd173b3f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62962627589d4836668e16733f8a1cfb9210fdd6")]
 [assembly: System.Reflection.AssemblyProductAttribute("HPlusSport.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HPlusSport.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
